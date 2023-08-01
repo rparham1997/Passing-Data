72525a8e35ff4aa03b31d866ddd0f123aaf43f87
rparham1997 <rparham1997@gmail.com>
2023-08-01 00:20:17 -0400

    .

--- DelegateProtocolUIkit/AddContactController.swift
+++ DelegateProtocolUIkit/AddContactController.swift
@@ -5,4 +5,61 @@
 //  Created by Ramar Parham on 7/31/23.
 //
 
-import Foundation
+import UIKit
+
+protocol AddContactDelegate {
+    func addContact(contact: Contact)
+}
+
+class AddContactController: UIViewController {
+    
+    var delegate: AddContactDelegate?
+    
+    let textField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "full Name"
+        tf.textAlignment = .center
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        view.backgroundColor = .red
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(handleDone))
+        
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(handleCancel))
+        
+        view.addSubview(textField)
+        textField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        textField.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        textField.widthAnchor.constraint(equalToConstant: view.frame.width - 64).isActive = true
+        
+        textField.becomeFirstResponder()
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleDone() {
+        
+        guard let fullname = textField.text, textField.hasText else {
+            print("Handle error here")
+            return
+        }
+        
+        let contact = Contact(fullname: fullname)
+        print(contact.fullname)
+        
+        delegate?.addContact(contact: contact)
+    }
+    
+    @objc func handleCancel() {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+
+}
+
--- DelegateProtocolUIkit/ContactsController.swift
+++ DelegateProtocolUIkit/ContactsController.swift
@@ -5,4 +5,61 @@
 //  Created by Ramar Parham on 7/31/23.
 //
 
-import Foundation
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+struct Contact {
+    var fullname: String
+}
+
+class ContactsController: UITableViewController {
+    
+    var contacts = [Contact]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationItem.title = "Contacts"
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(handleAddContact))
+        
+        view.backgroundColor = .blue
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleAddContact() {
+        
+        let controller = AddContactController()
+        controller.delegate = self
+        
+        self.present(UINavigationController(rootViewController: controller), animated: true, completion: nil)
+    }
+    
+    // MARK: - UITableView
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.textLabel?.text = contacts[indexPath.row].fullname
+        return cell
+    }
+
+}
+
+extension ContactsController: AddContactDelegate {
+    
+    func addContact(contact: Contact) {
+        self.dismiss(animated: true)
+        self.contacts.append(contact)
+        self.tableView.reloadData()
+    }
+}
+
